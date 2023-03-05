375a09d9d83d11d5598af48eb70089c0452a6b1e
Noora <abdounoora38@gmail.com>
2023-03-05 16:16:30 -0500

    added description and icon

--- src/index.js
+++ src/index.js
@@ -13,23 +13,34 @@ let days = [
 ];
 currentDate.innerHTML = `${days[2]} ${hours}:${minutes}`;
 
-function search(event) {
-  event.preventDefault();
-  let h1 = document.querySelector("#city");
-  let places = document.querySelector("#city-input");
-  h1.innerHTML = places.value;
-  let apiKey = "ebef9ca4a8de66ed586fac628fade056";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${places.value}&appid=${apiKey}&units=metric`;
-  axios.get(`${apiUrl}`).then(callbackApi);
-}
-
-let cities = document.querySelector("#city-form");
-cities.addEventListener("submit", search);
-
 function callbackApi(response) {
   let temperature = document.querySelector("#degree");
-  temperature.innerHTML = Math.round(response.data.main.temp);
   let description = document.querySelector("#clear");
+  let windElement = document.querySelector("#wind");
+  let humididtyElement = document.querySelector("#humidity");
+  let cityElement = document.querySelector("#city");
+  let iconElement = document.querySelector("#icon");
+  temperature.innerHTML = Math.round(response.data.main.temp);
   description.innerHTML = response.data.weather[0].description;
+  cityElement.innerHTML = response.data.name;
+  humididtyElement.innerHTML = `Humidity ${response.data.main.humidity}%`;
+  windElement.innerHTML = `Wind ${Math.round(response.data.wind.speed)} km/h`;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
-console.log(response.data.weather[0].description);
+function search(city) {
+  let apiKey = "ebef9ca4a8de66ed586fac628fade056";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(callbackApi);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#city-form");
+form.addEventListener("submit", handleSubmit);
